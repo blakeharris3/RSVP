c13091e97dbe1740c1c58d52371a9782f06ce1b2
Blake Harris <Blake@Blakes-MBP.local>
2019-05-14 00:29:20 -0700

    EventContainer setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import EventContainer from './EventContainer';
+
+import Header from './Header';
+import { Route, Switch } from 'react-router-dom';
+
+const My404 = () => {
+  return (
+    <div>
+      You were re-directed to bad page!
+    </div>
+    )
+}
+
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-      <Header />
-        
+        <Header />
+        <Switch>
+          <Route exact path="/events" component={EventContainer}/>
+          <Route component={My404}/>
+        </Switch>
       </div>
     );
   }
